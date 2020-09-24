ae669bfc7f4e8df9d0eaf6c0ea5c8f86f3264b16
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-09-24 10:15:29 -0400

    Base structure of the project.

---
+++ app/src/main/java/bo/com/emprendeya/repository/Repository.java
@@ -0,0 +1,22 @@
+package bo.com.emprendeya.repository;
+
+public class Repository implements RepositoryImpl {
+
+    /*
+    private static Repository instance;
+
+    public static Repository getInstance() {
+        if (instance == null) {
+            instance = new Repository();
+        }
+        return instance;
+    }
+
+    private static String message = "Hola Mundo";
+
+    public static boolean login() {
+        return true;
+    }
+    */
+
+}
