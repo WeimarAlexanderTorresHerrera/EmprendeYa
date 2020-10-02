aaf0392393d6a1602476826f68901bb99aaed998
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-10-02 15:56:44 -0400

    Base> TDD finish tests.

---
+++ app/src/main/java/bo/com/emprendeya/utils/Validations.java
@@ -0,0 +1,14 @@
+package bo.com.emprendeya.utils;
+
+import java.util.regex.Pattern;
+
+public class Validations {
+    public static boolean isEmpty(String target) {
+        return target.isEmpty();
+    }
+
+    public static boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+        return pattern.matcher(email).matches();
+    }
+}
